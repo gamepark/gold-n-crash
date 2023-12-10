9a778a75822c870944019b5467b62d554f0015bc
Patrick Beaumé <p.beaume@gmail.com>
2023-12-10 22:52:34 +0100

    Initial commit

---
+++ app/src/GameDisplay.tsx
@@ -0,0 +1,16 @@
+/** @jsxImportSource @emotion/react */
+import { GameTable } from '@gamepark/react-game'
+import { FC } from 'react'
+import { PlayerPanels } from './panels/PlayerPanels'
+
+type GameDisplayProps = {
+  players: number
+}
+
+export const GameDisplay: FC<GameDisplayProps> = () => {
+  return <>
+    <GameTable xMin={-50} xMax={50} yMin={-30} yMax={30}
+               margin={{ top: 7, left: 0, right: 30, bottom: 0 }}/>
+    <PlayerPanels/>
+  </>
+}
