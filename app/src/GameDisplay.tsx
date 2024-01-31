072fda439a5d3a2c519da13bd0816ab6314e32d1
Patrick Beaume <p.beaume@gmail.com>
2024-01-31 13:43:36 +0100

    remove game table color

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -1,9 +1,8 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react'
 //import { css } from '@emotion/react'
 import { GameTable, GameTableNavigation, usePlayerId } from '@gamepark/react-game'
-import {FC} from 'react'
-import {PlayerPanels} from './panels/PlayerPanels'
+import { FC } from 'react'
+import { PlayerPanels } from './panels/PlayerPanels'
 
 type GameDisplayProps = {
   players: number
@@ -18,7 +17,7 @@ export const GameDisplay: FC<GameDisplayProps> = () => {
       yMin={!playerId? -34: -30}
       yMax={34}
       margin={{ top: 7, left: 0, right: 0, bottom: 0 }}
-      css={css`background-color: rgba(255, 255, 255, 0.7)`}
+      //css={css`background-color: rgba(255, 255, 255, 0.7)`}
     >
       <GameTableNavigation />
     </GameTable>
