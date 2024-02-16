f3f5f72e0c0f8eb8903d8d710561dfd2305c19c0
Patrick Beaume <p.beaume@gmail.com>
2024-02-16 10:16:53 +0100

    use action history from framework

--- app/src/history/entry/ActionHistory.tsx
+++ app/src/history/entry/ActionHistory.tsx
@@ -1,69 +1,17 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react'
 import { Flag } from '@gamepark/gold-n-crash/material/Flag'
-import { HistoryEntry, HistoryEntryContext, Picture, PlayerHistoryEntry } from '@gamepark/react-game'
+import { ActionHistoryEntry, ActionHistoryEntryProps } from '@gamepark/react-game'
 import { FC } from 'react'
 import { playerColorCode } from '../../panels/PlayerPanels'
 
-type ActionHistoryProps = {
-  consequence?: boolean
-  depth?: number
-  picture?: string
-  pictureCss?: any
-  context: HistoryEntryContext
-}
 
-
-export const ActionHistory: FC<ActionHistoryProps> = (props) => {
-  const { consequence, depth, picture, pictureCss, context, children } = props
-  const Component = consequence? HistoryEntry: PlayerHistoryEntry
+export const ActionHistory: FC<ActionHistoryEntryProps> = (props) => {
+  const { context, children, ...rest } = props
   return (
-    <Component context={context} css={[color(context.action.playerId)]}>
-      <div css={flex}>
-        {consequence && (
-          <div css={consequenceIcon(depth)}>⤷</div>
-        )}
-        <div css={growth}>
-          {children}
-        </div>
-        {picture && (
-          <div css={actionPicture}>
-            <Picture css={[pictureStyle, pictureCss]} src={picture}/>
-          </div>
-        )}
-      </div>
-    </Component>
+    <ActionHistoryEntry context={context} getColor={getColor} {...rest}>
+      {children}
+    </ActionHistoryEntry>
   )
 }
 
-const flex = css`
-  display: flex;
-  width: 100%;
-  align-items: center;
-`
-
-const growth = css`
-  flex: 1;
-`
-
-const pictureStyle = css`
-  height: 2.2em; 
-  border-radius: 0.5em; 
-  border: 0.1em solid black
-`
-
-const color = (flag: Flag) => css`
-  background-color: ${playerColorCode[flag]}20;
-`
-
-const actionPicture = css`
-  padding-left: 0.3em;
-  border-radius: 0.5em;
-`
-
-const consequenceIcon = (times: number = 1) => css`
-  font-size: 1.5em;
-  margin-right: 0.4em;
-  margin-left: ${0.5 + (1.5 * (times - 1))}em;
-  margin-top: -0.2em;
-`
+const getColor = (flag: Flag) => playerColorCode[flag]
